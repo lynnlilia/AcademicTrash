65adfcd4e36f07b8897e1029764d11df71fa6e83
lynnlilia <132737961+lynnlilia@users.noreply.github.com>
2023-05-11 17:05:39 +0800

    Update study.demo.spec.js

--- study.demo.spec.js
+++ study.demo.spec.js
@@ -2,37 +2,56 @@ const { test, expect } = require('@playwright/test');
 
 test('has nothing but title', async ({ page }) => {
   await page.goto('https://gadero.vercel.app/winkelwagen/');
-  await page.waitForTimeout(2000)
+
   await page.fill("input[name= '_vercel_password']","gadero2023")
-  await page.waitForTimeout(2000)
   await page.getByRole('button', { hasText: 'Log in' }).click();
-  await page.waitForTimeout(2000)
+
   await page.getByText('Je winkelwagen is leeg');
-  //await page.locator('container mx-auto pb-12 sm:pb-24',{ hasText :'Je winkelwagen is leeg'}); 
+ 
   });
   
 test('shopping', async ({page})=>{
   await page.goto('https://gadero.vercel.app/schuttingdeur-geimpregneerd-in-stalen-frame-130-x-195-cm-met-rvs-inbouwcilinderslot-en-ophangogen/');
-  //await page.getByRole('button', { name: 'Close' }).click();
+  
   await page.fill("input[name= '_vercel_password']","gadero2023")
   await page.getByRole('button', { hasText: 'Log in' }).click();
-  await page.getByLabel('Schuttingdeur geïmpregneerd in stalen frame 130 x 195 cm met RVS inbouwcilinderslot en ophangogen-1 counter')
-  .fill('5');
-  await page.getByLabel('Schuttingdeur geïmpregneerd in stalen frame 130 x 195 cm met RVS inbouwcilinderslot en ophangogen-6 counter')
-  .fill('3');
-  await page.getByRole('button', { name: 'Voeg toe aan mijn winkelwagen' }).click();
-  //await page.getByRole('button', { name: 'sluiten' }).click()
-  
-  await page.locator('"text-[10px] -top-0.5"',{ hasText :'8'});
-  await page.getByRole('button', { name: 'Ga naar winkelwagen' }).click();
-  await page.getByPlaceholder('Vul hier je postcode en huisnummer in').fill('1441ek');
-  
 
-  //await page.getByText('1441EK Purmerend, Wilhelminalaan 1', { exact: true }).click();
+  const cookie=page.getByRole('button', { name: 'Alles accepteren' }) 
+  await cookie.waitFor({state:'attached'})
+  await cookie.click()
+
+
+  await page.getByLabel('Schuttingdeur geïmpregneerd in stalen frame 130 x 195 cm met RVS inbouwcilinderslot en ophangogen-90 cm Aantal')
+  .fill('5',{force:true});
+  await page.getByLabel('Schuttingdeur geïmpregneerd in stalen frame 130 x 195 cm met RVS inbouwcilinderslot en ophangogen-130 cm Aantal')
+  .fill('3',{force:true});
+  
+  await page.getByRole('button', { name: 'Voeg toe aan mijn winkelwagen' }).click({force:true})
+  const cartlogo=await page.getByRole('link', { name: '8 Winkelwagen' })
+  await cartlogo.waitFor({state:"attached"});
+
+  const gotocart = await page.getByText('Ga naar winkelwagen')
+  await gotocart.waitFor({state:"attached"});
+  await gotocart.click({force:true})
+  await expect(page).toHaveURL('https://gadero.vercel.app/winkelwagen/')
+
+ 
+  await page.getByPlaceholder('Vul hier je postcode en huisnummer in').fill('1441ek');
   await page.getByRole('listitem').filter({ hasText: '1441ek' }).nth(0).click();
 
-  await page.getByRole('radio', { name: 'Afhalen bij Gadero' }).click();
-  await page.getByRole('radio', { name: 'Winkel vrijdag 19 mei (20:00-00:00) 9,95' }).click();
-  await page.getByRole('button', { name: 'Ga naar de volgende stap' }).click();
+  const radio=page.getByRole('radio', { name: 'Afhalen bij Gadero'}) 
+  await radio.waitFor({state:'attached'})
+  await radio.click()
+
+  const radio1=page.getByRole('radiogroup').nth(0).click();
+
+  const button1=page.getByRole('button', { name: 'Ga naar de volgende stap' }) 
+  await button1.waitFor({state:'attached'})
+  await button1.click()
+
+  await expect(page).toHaveURL('https://gadero.vercel.app/checkout/customer-details/')
+  
 })
  
+
+  
